2b22c2a70631e81e62e6e8b4daeb589c9e508eb5
NikeshMaharjan1217 <38070089+NikeshMaharjan1217@users.noreply.github.com>
2018-04-08 10:36:25 +0545

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,13 +2,10 @@
 #include<conio.h>
 main()
 {
-	int a,b,c,S,S1,area;
-	a=10;
-	b=12;
-	c=15;
-	S=(a+b+c)/2;
-	S1=(S*(S-a)*(S-b)*(S-c));
-	area=sqrt(S1)
-	printf("Area of a triangle is %d",area);
+	int a;
+	printf("entre a ASCI number:");
+	scanf("%d",&a);
+	printf("ASCII value of %d=%c",a,a);
 	getch();
+		
 }
