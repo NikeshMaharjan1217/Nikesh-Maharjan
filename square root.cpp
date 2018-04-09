b2f2d4dc8e668212ca4d33a2acb31d85cc6c0eae
NikeshMaharjan1217 <38070089+NikeshMaharjan1217@users.noreply.github.com>
2018-04-09 10:44:23 +0545

    Lab 2

---
+++ square root.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+main()
+{
+	float a,s;
+	printf("Enter the value of a:");
+	scanf("%f",&a);
+	s=sqrt(a);
+	printf("Square root of a is %.2f",s);
+	getch();
+}
