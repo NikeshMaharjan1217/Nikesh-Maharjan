dda7d0f2f269e72e926ad917984b4f4591c91692
NikeshMaharjan1217 <38070089+NikeshMaharjan1217@users.noreply.github.com>
2018-04-15 10:49:45 +0545

    Lab 3

---
+++ Relational Operation.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b;
+	printf("Enter the value of a and b:\n");
+	scanf("%d%d",&a,&b);
+	printf("%d<%d is %d",a,b,a<b);
+	printf("\n%d<=%d is %d",a,b,a<=b);
+	printf("\n%d>%d is %d",a,b,a>b);
+	printf("\n%d>=%d is %d",a,b,a>=b);
+	printf("\n%d==%d is %d",a,b,a==b);
+	printf("\n%d!=%d is %d",a,b,a!=b);
+	getch();
+}
