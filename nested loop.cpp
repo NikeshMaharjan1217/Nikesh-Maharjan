ce94d8bf9fa09a7ce367e16169a41820a21bdc40
NikeshMaharjan1217 <38070089+NikeshMaharjan1217@users.noreply.github.com>
2018-06-24 10:35:26 +0545

    Add files via upload

---
+++ nested loop.cpp
@@ -0,0 +1,18 @@
+//lab11  june24
+// nested for loop
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n=5,i=1,j=1,count=0;
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+			
+		}
+		printf("\n");
+	
+	}
+}
